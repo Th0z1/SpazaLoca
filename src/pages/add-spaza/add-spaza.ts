a30894997ec98b6f4527eea954e7dc3f2c07b60d
realSanele <therealsanele@gmail.com>
2019-01-16 13:52:44 +0200

    Add spaza page added v2.0

--- src/pages/add-spaza/add-spaza.ts
+++ src/pages/add-spaza/add-spaza.ts
@@ -7,6 +7,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+declare var firebase;
 
 @IonicPage()
 @Component({
@@ -14,12 +15,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'add-spaza.html',
 })
 export class AddSpazaPage {
+  spazaName:string;
+  city:string;
+  streetName:string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddSpazaPage');
+  }
+
+  addSpaza(){
+    var currentUser = firebase.auth().currentUser;
+
+    firebase.database().ref("/users/"+currentUser.uid+"/mySpazas/").push(
+     {
+      spazaName: this.spazaName,
+      city: this.city,
+      streetName: this.streetName,
+     } 
+    );
+
   }
 
 }
