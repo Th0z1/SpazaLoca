00e68486bfa7a233cce5fcda119cb1c77e757ccb
realSanele <therealsanele@gmail.com>
2019-01-16 10:25:43 +0200

    Add spaza page added

---
+++ src/pages/add-spaza/add-spaza.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the AddSpazaPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-add-spaza',
+  templateUrl: 'add-spaza.html',
+})
+export class AddSpazaPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AddSpazaPage');
+  }
+
+}
---
+++ src/pages/my-stores/my-stores.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the MyStoresPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-my-stores',
+  templateUrl: 'my-stores.html',
+})
+export class MyStoresPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MyStoresPage');
+  }
+
+  addSpaza(){
+    console.log("Clicked")
+    this.navCtrl.push('AddSpazaPage');
+  }
+
+}
